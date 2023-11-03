a9931b8aa55ec277b3be4afc6241c838903249da
blokboy <proxxist@gmail.com>
2023-11-03 18:23:50 -0400

    links for selecting genres for sounds

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import NavLinks from "@/ui/dashboard/nav-links"
 import Search from "@/ui/dashboard/search"
 
 export default function Page() {
     return (
-      <main>
+      <main className="p-1">
+        <div>
         <Search placeholder="Search mine.fm..."/>
+        </div>
+        <div className="m-3">
+          <NavLinks />
+        </div>
+        
+        
       </main>
       
     )
